3f3ab5af0bff917ef9d2480bc86e047fda54649c
Nathan Malkin <nmalkin@cs.berkeley.edu>
2022-02-05 22:50:11 -0800

    Update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     "extends": ["prettier"],
     "rules": {
         "no-extend-native": "off",
-        "no-unused-vars": "warn",
+        "no-unused-vars": ["error", { "vars": "local", "args": "after-used", "ignoreRestSiblings": false }],
         "no-var": "off",
         "require-jsdoc": "off",
         "valid-jsdoc": "off"
