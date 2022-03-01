5c6f5f48af32459b08ce273b073a548da48142d3
Nathan Malkin <nmalkin@cs.berkeley.edu>
2022-03-01 12:19:21 -0500

    Add utility methods and types

--- src/Cell.ts
+++ src/Cell.ts
@@ -1,4 +1,6 @@
 type Cell = number | string | Date;
+type NonDateCell = number | string;
+
 type CellRange = Array<Array<Cell>>;
 type CellOrRange = Cell | CellRange;
 
@@ -14,4 +16,17 @@ function filterEmpty_(array: string[]) {
 
 function getCodesInCell_(cell: Cell): string[] {
   return filterEmpty_(cell.toString().split(CODES_SEPARATOR)).unique();
+}
+
+function cellIsDate_(cell: Cell): cell is NonDateCell {
+  return !(cell instanceof Date);
+}
+
+function validateCells_(cells: Cell[]): NonDateCell[] {
+  return cells.map((cell) => {
+    if (cell instanceof Date) {
+      throw new QcasError(`code ${cell} is unexpected type: Date`);
+    }
+    return cell;
+  });
 }
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,12 @@
 function isString(data: unknown): data is string {
   return typeof data === 'string';
 }
+
+/**
+ * via https://stackoverflow.com/a/46700791
+ */
+function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
+  if (value === null || value === undefined) return false;
+  const testDummy: TValue = value;
+  return true;
+}
