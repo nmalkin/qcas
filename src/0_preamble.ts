c536cca91b25bdfb4a6fe87f3cadfbf904ebef8b
Nathan Malkin <nmalkin@cs.berkeley.edu>
2022-04-06 23:25:27 -0700

    Release 0.3.0

--- src/0_preamble.ts
+++ src/0_preamble.ts
@@ -17,4 +17,4 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-const VERSION = 'v0.2.0-20220406';
+const VERSION = 'v0.3.0';
