fe99b9f710749a2483f01cff5bd553ca13e9e799
Nathan Malkin <nmalkin@cs.berkeley.edu>
2024-03-25 17:36:44 -0400

    Release 0.3.1

--- src/0_preamble.ts
+++ src/0_preamble.ts
@@ -2,7 +2,7 @@
 QCAS
 https://github.com/nmalkin/qcas
 
-Copyright (C) 2022 N. Malkin
+Copyright (C) 2024 N. Malkin
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
@@ -17,4 +17,4 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-const VERSION = 'v0.3.0';
+const VERSION = 'v0.3.1';
